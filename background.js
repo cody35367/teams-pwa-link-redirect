9c85ad245901eb556e7a0c697728ea00dc564471
Cody Hodges <chodges@heliossolutionsllc.com>
2025-11-18 17:54:52 -0600

    Update to work for my use-case.

--- background.js
+++ background.js
@@ -17,9 +17,24 @@ chrome.runtime.onInstalled.addListener(() => {
             "teams.microsoft.com",
             "login.microsoftonline.com",
             "outlook.office.com",
+            "teams.microsoft.us",
+            "gov.teams.microsoft.us"
           ],
         },
       },
     ],
   });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  // Define the URL that should trigger closing the tab
+  const targetUrl = "https://statics.gov.teams.microsoft.us/evergreen-assets/safelinks/1/atp-safelinks.html"; // Change this to the URL you want to match
+
+  // Check if the tab has finished loading and if the URL matches the target
+  if (changeInfo.status === "complete" && tab.url === targetUrl) {
+
+    setTimeout(function() {
+      chrome.tabs.remove(tabId);
+    }, 1000);
+  }
 });
