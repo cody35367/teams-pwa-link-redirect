e0e461d25247b3dbdcfcee45524d480f84579aba
Cody Hodges <chodges@intuitivemachines.com>
2025-12-01 13:28:44 -0600

    Enhance tab cleanup options

--- background.js
+++ background.js
@@ -31,13 +31,24 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // Define the URL that should trigger closing the tab
-  const targetUrl = "https://statics.gov.teams.microsoft.us/evergreen-assets/safelinks/2/atp-safelinks.html"; // Change this to the URL you want to match
-
+  const targetUrls = ["statics.gov.teams.microsoft.us/evergreen-assets/safelinks"]; // Change this to the URL you want to match
+  const tabUrlLower = (tab.url) ? tab.url.toLowerCase(): "";
   // Check if the tab has finished loading and if the URL matches the target
-  if (changeInfo.status === "complete" && tab.url === targetUrl) {
-
+  if (changeInfo.status === "complete" && tab.url && targetUrls.some(targetUrl => tabUrlLower.includes(targetUrl.toLowerCase()))) {
     setTimeout(function() {
       chrome.tabs.remove(tabId);
     }, 1000);
   }
 });
+
+// The Polling Loop (Catch-all)
+// This runs every 2 seconds to find any tabs we missed
+setInterval(() => {
+  chrome.tabs.query({}, (tabs) => {
+    for (const tab of tabs) {
+      if (tab.url === "" && tab.status === 'complete') { // This will clean up all extra about:blanks
+        chrome.tabs.remove(tab.id);
+      }
+    }
+  });
+}, 2000); // Check every 2 seconds
